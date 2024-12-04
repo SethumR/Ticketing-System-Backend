abb7a361bd24709c779f72f1d84d20c7ff333f26
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2024-12-05 03:36:43 +0530

    First Commit

--- src/main/java/com/sethum/ticketingbackend/controller/ConfigurationController.java
+++ src/main/java/com/sethum/ticketingbackend/controller/ConfigurationController.java
@@ -11,35 +11,5 @@ public class ConfigurationController {
     private Thread simulationThread;
     private boolean isRunning = false;
 
-    @PostMapping("/start")
-    public String startSimulation() {
-        if (isRunning) {
-            return "Simulation is already running!";
-        }
 
-        isRunning = true;
-        simulationThread = new Thread(() -> {
-            try {
-                while (isRunning) {
-                    Thread.sleep(configuration.getTicketReleaseRate());
-//                    Ticket ticket = new Ticket();
-//                    ticketService.saveTicket(ticket);
-//                    System.out.println("Ticket released!");
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        });
-        simulationThread.start();
-        return "Simulation started!";
-    }
-
-    @PostMapping("/stop")
-    public String stopSimulation() {
-        isRunning = false;
-        if (simulationThread != null) {
-            simulationThread.interrupt();
-        }
-        return "Simulation stopped!";
-    }
 }
