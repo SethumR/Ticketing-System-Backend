8200f59705d33693eb5a51c7cee77e0935210de9
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2024-12-06 01:42:47 +0530

    Added sent values from the frontend

--- src/main/java/com/sethum/ticketingbackend/controller/ConfigurationController.java
+++ src/main/java/com/sethum/ticketingbackend/controller/ConfigurationController.java
@@ -3,6 +3,7 @@ package com.sethum.ticketingbackend.controller;
 import com.sethum.ticketingbackend.model.Configuration;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5174") //
 @RestController
 @RequestMapping("/api/config")
 public class ConfigurationController {
@@ -12,34 +13,46 @@ public class ConfigurationController {
     private boolean isRunning = false;
 
     @PostMapping("/start")
-    public String startSimulation() {
-        if (isRunning) {
-            return "Simulation is already running!";
-        }
+    public String startSimulation(@RequestBody SimulationConfig config) {
+        // Print the configuration in the terminal
+        System.out.println("Received Configuration:");
+        System.out.println("Total Tickets: " + config.getTotalTickets());
+        System.out.println("Release Rate: " + config.getReleaseRate());
+        System.out.println("Retrieval Rate: " + config.getRetrievalRate());
+        System.out.println("Max Capacity: " + config.getMaxCapacity());
 
-        isRunning = true;
-        simulationThread = new Thread(() -> {
-            try {
-                while (isRunning) {
-                    Thread.sleep(configuration.getTicketReleaseRate());
-//                    Ticket ticket = new Ticket();
-//                    ticketService.saveTicket(ticket);
-//                    System.out.println("Ticket released!");
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        });
-        simulationThread.start();
-        return "Simulation started!";
+        return "Simulation started with the given configuration";
     }
 
-    @PostMapping("/stop")
-    public String stopSimulation() {
-        isRunning = false;
-        if (simulationThread != null) {
-            simulationThread.interrupt();
-        }
-        return "Simulation stopped!";
-    }
+//    @PostMapping("/start")
+//    public String startSimulation() {
+//        if (isRunning) {
+//            return "Simulation is already running!";
+//        }
+//
+//        isRunning = true;
+//        simulationThread = new Thread(() -> {
+//            try {
+//                while (isRunning) {
+//                    Thread.sleep(configuration.getTicketReleaseRate());
+////                    Ticket ticket = new Ticket();
+////                    ticketService.saveTicket(ticket);
+////                    System.out.println("Ticket released!");
+//                }
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//            }
+//        });
+//        simulationThread.start();
+//        return "Simulation started!";
+//    }
+//
+//    @PostMapping("/stop")
+//    public String stopSimulation() {
+//        isRunning = false;
+//        if (simulationThread != null) {
+//            simulationThread.interrupt();
+//        }
+//        return "Simulation stopped!";
+//    }
 }
