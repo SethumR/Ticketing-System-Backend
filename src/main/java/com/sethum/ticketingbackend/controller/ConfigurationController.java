6124ef6efef1f56b9f5fc0aa37cb9859f70292fd
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2024-12-08 16:20:24 +0530

    Added my frontend local host to the backend

--- src/main/java/com/sethum/ticketingbackend/controller/ConfigurationController.java
+++ src/main/java/com/sethum/ticketingbackend/controller/ConfigurationController.java
@@ -1,58 +1,23 @@
 package com.sethum.ticketingbackend.controller;
 
 import com.sethum.ticketingbackend.model.Configuration;
+import com.sethum.ticketingbackend.service.ConfigurationService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5174") //
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/config")
 public class ConfigurationController {
 
-    private Configuration configuration = new Configuration();
-    private Thread simulationThread;
-    private boolean isRunning = false;
+    private final ConfigurationService simulationService = new ConfigurationService();
 
     @PostMapping("/start")
-    public String startSimulation(@RequestBody SimulationConfig config) {
-        // Print the configuration in the terminal
-        System.out.println("Received Configuration:");
-        System.out.println("Total Tickets: " + config.getTotalTickets());
-        System.out.println("Release Rate: " + config.getReleaseRate());
-        System.out.println("Retrieval Rate: " + config.getRetrievalRate());
-        System.out.println("Max Capacity: " + config.getMaxCapacity());
-
-        return "Simulation started with the given configuration";
+    public String startSimulation(@RequestBody Configuration config) {
+        return simulationService.startSimulation(config);
     }
 
-//    @PostMapping("/start")
-//    public String startSimulation() {
-//        if (isRunning) {
-//            return "Simulation is already running!";
-//        }
-//
-//        isRunning = true;
-//        simulationThread = new Thread(() -> {
-//            try {
-//                while (isRunning) {
-//                    Thread.sleep(configuration.getTicketReleaseRate());
-////                    Ticket ticket = new Ticket();
-////                    ticketService.saveTicket(ticket);
-////                    System.out.println("Ticket released!");
-//                }
-//            } catch (InterruptedException e) {
-//                Thread.currentThread().interrupt();
-//            }
-//        });
-//        simulationThread.start();
-//        return "Simulation started!";
-//    }
-//
-//    @PostMapping("/stop")
-//    public String stopSimulation() {
-//        isRunning = false;
-//        if (simulationThread != null) {
-//            simulationThread.interrupt();
-//        }
-//        return "Simulation stopped!";
-//    }
+    @PostMapping("/stop")
+    public String stopSimulation() {
+        return simulationService.stopSimulation();
+    }
 }
