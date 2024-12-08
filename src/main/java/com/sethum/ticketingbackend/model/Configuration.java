f91597b5f57b5b33ab9feae192cfaf613a07e02d
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2024-12-08 16:21:26 +0530

    Created getters setters for the user inputs (@Data)

--- src/main/java/com/sethum/ticketingbackend/model/Configuration.java
+++ src/main/java/com/sethum/ticketingbackend/model/Configuration.java
@@ -1,19 +1,15 @@
 package com.sethum.ticketingbackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity
 public class Configuration {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private int totalTickets;
-    private int ticketReleaseRate;
-    private int customerRetrievalRate;
-    private int maxTicketCapacity;
+
+    private String TotalTickets;
+    private String ReleaseRate;
+    private String RetrievalRate;
+    private String MaxCapacity;
+    private String numofVendors;
+    private String numofCustomers;
+
 }
